c9502ed3a4dfed366209f45122f09d50e0c4d22b
vijayantajain <vijayantashah@gmail.com>
2018-10-08 02:12:35 -0500

    Implement testing structure

--- src/stats.js
+++ src/stats.js
@@ -1,17 +1,19 @@
 //Module containing some statistical functions to be used by `argus` module
 
-/**
- * Returns the statistical mean of the input array of data.
- * 
- * @param {Object} arrOfData Array of data over which average or statiscal mean
- *                          is to be calculated
- */
-function average(arrOfData) {
-    let tempSum  = 0;
-
-    for (items in arrOfData) {
-        tempSum += arrOfData[items];
+module.exports = {
+    /**
+     * Returns the statistical mean of the input array of data.
+     * 
+     * @param {Object} arrOfData Array of data over which average or statiscal mean
+     *                          is to be calculated
+     */
+    average: function average(arrOfData) {
+        let tempSum  = 0;
+    
+        for (items in arrOfData) {
+            tempSum += arrOfData[items];
+        }
+    
+        return tempSum / arrOfData.length;
     }
-
-    return tempSum / arrOfData.length;
-}
+};
