62dac30c04940bf38e666bcdd932fe0c0764a5c6
ppradiplamsal <pradip.lamsal@ttu.edu>
2018-10-09 15:47:24 -0500

    Added max and min functions and their corresponding test functions

--- src/stats.js
+++ src/stats.js
@@ -4,8 +4,7 @@ module.exports = {
     /**
      * Returns the statistical mean of the input array of data.
      * 
-     * @param {Object} arrOfData Array of data over which average or statiscal mean
-     *                          is to be calculated
+     * @param {Object} arrOfData Array of data over which values of various statistical functions are to be calculated
      */
     average: function average(arrOfData) {
         let tempSum  = 0;
@@ -16,4 +15,25 @@ module.exports = {
     
         return tempSum / arrOfData.length;
     }
+	
+	max: function max(arrOfData) {
+		let max = arrOfData[0];
+		
+		for (items in arrOfData) {
+			if (arrOfData[items] > max) max = arrOfData[items];
+		}
+		
+		return max;
+	}
+		
+	min: function min(arrOfData) {
+		let min = arrOfData[0];
+		
+		for (items in arrOfData) {
+			if (arrOfData[items] < min) min = arrOfData[items];
+		}
+		
+		return min;
+	}		
+		
 };
--- test/testStats.js
+++ test/testStats.js
@@ -2,10 +2,25 @@
 
 var statsModule = require('..\\src\\stats.js');
 var average = statsModule.average;
+var max = statsModule.max;
+var min = statsModule.min;
 var assert = require('assert');
 
 describe('average', function () {
-    it('should return the average of input array', function () {
+    it('should return the average of the input array', function () {
         assert.equal(2, average([1, 2, 3]));
     });
 });
+
+describe('max', function () {
+    it('should return the maximum value in the input array', function () {
+        assert.equal(62, max([13, 62, 9]));
+    });
+});
+
+describe('min', function () {
+    it('should return the minimum value in the input array', function () {
+        assert.equal(9, min([13, 62, 9]));
+    });
+});
+
