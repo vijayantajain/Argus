f917dc66c7cdf7f4827f331a59d2c5ad9a784370
vijayantajain <vijayantashah@gmail.com>
2018-10-10 22:22:34 -0500

    Add docstrings for the functions

--- src/stats.js
+++ src/stats.js
@@ -4,35 +4,53 @@ module.exports = {
     /**
      * Returns the statistical mean of the input array of data.
      * 
-     * @param {Object} arrOfData Array of data over which values of various statistical functions are to be calculated
+     * @param {Object} arrOfData Array of data over which values of various
+	 * 							 statistical functions are to be calculated
+	 * 
+	 * @returns {Double}         Returns the statistical mean of `arrOfData`
      */
     average: function average(arrOfData) {
         let tempSum  = 0;
-    
+
         for (items in arrOfData) {
             tempSum += arrOfData[items];
         }
-    
+
         return tempSum / arrOfData.length;
-    },
-	
+	},
+
+	/**
+	 * 
+	 * @param {Object} arrOfData Array of data from the which the largest 
+	 * 							 element is found
+	 *
+	 * @returns {Double}         The largest element in the `arrOfData`
+	 */
+
 	max: function max(arrOfData) {
 		let max = arrOfData[0];
 		
 		for (items in arrOfData) {
 			if (arrOfData[items] > max) max = arrOfData[items];
 		}
-		
+
 		return max;
 	},
-		
+	
+	/**
+	 * 	
+	 * @param {Object} arrOfData Array of data from which the smallest element
+	 * 							 is obtained
+	 * 
+	 * @returns {Double}         The smallest element in `arrOfData`
+	 */
 	min: function min(arrOfData) {
 		let min = arrOfData[0];
 		
 		for (items in arrOfData) {
 			if (arrOfData[items] < min) min = arrOfData[items];
 		}
-		
+
 		return min;
 	}		
 		
