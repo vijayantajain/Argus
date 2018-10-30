5582f114965fdd5062fdfe20eab56e61cca9c5ac
zluckman <zack.luckman@ttu.edu>
2018-10-30 13:48:15 -0500

    Update src/argus.js
    Average for each attribute working

--- src/argus.js
+++ src/argus.js
@@ -12,16 +12,22 @@ const listOfNodes = Object.getOwnPropertyNames(dataFile);
 //TODO
 //Assert that an argument is passed!
 console.log('Calculating  ' + process.argv[3]);
-
+var x=0;
+var divide_me=0;
 //TODO
 // Print Data
 for (node in listOfNodes){
     console.log("The data for "+listOfNodes[node]+":");
     for (att in dataFile[listOfNodes[node]]){
+        x=0;
+        divide_me=0;
         console.log("The data for "+att+" at "+listOfNodes[node] +":");
         for (num in dataFile[listOfNodes[node]][att]){
+            if (dataFile[listOfNodes[node]][att][num]!=null){x+=1;}
             console.log(dataFile[listOfNodes[node]][att][num]);
+            divide_me+=dataFile[listOfNodes[node]][att][num];
         }
+        console.log("average:"+divide_me+"/"+x+"="+(divide_me/x).toFixed(6));
     }
 }
 
