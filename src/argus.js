35d5f1dae32660263cd27d68695e61d4821b6a11
vijayantajain <vijayantashah@gmail.com>
2018-11-12 00:11:43 -0600

    Change var to const or let

--- src/argus.js
+++ src/argus.js
@@ -3,22 +3,22 @@
 const path = require('path');
 const CWD = __dirname;
 
-const NEGATIVE_INFINITY = -1e10;
-const POSITIVE_INFINITY =  1e10;
+const MINIMUM_POSSIBLE_VALUE = -1e10;
+const MAXIMUM_POSSIBLE_VALUE =  1e10;
 
 const statsModule = require(path.join('..', 'src', 'stats.js'));
-var average = statsModule.average;
-var max = statsModule.max;
-var min = statsModule.min;
-var sudden_change = statsModule.sudden_change;
-var variance = statsModule.variance;
-var min = statsModule.min;
+const average = statsModule.average;
+const max = statsModule.max;
+const min = statsModule.min;
+const sudden_change = statsModule.sudden_change;
+const variance = statsModule.variance;
+const min = statsModule.min;
 
 //TODO
 //Assert that the file path and the file name exists!
 const fileName = process.argv[2];
 let dataFile = require(path.join(CWD, '..', 'data', fileName));
-const listOfNodes = Object.getOwnPropertyNames(dataFile);
+const COMPUTE_NODES = Object.getOwnPropertyNames(dataFile);
 
 //Assert that the variables are correct
 console.log('Calculating  ' + process.argv[3]);
@@ -65,38 +65,42 @@ switch (target_variable) {
 }
 
 var vals;
-var max = NEGATIVE_INFINITY;
-var min = POSITIVE_INFINITY;
+var max = MINIMUM_POSSIBLE_VALUE;
+var min = MAXIMUM_POSSIBLE_VALUE;
 var reading;
 var equipment;
 
-for (node in listOfNodes){
+for (node in COMPUTE_NODES){
 
-    for (att in dataFile[listOfNodes[node]]){
+    for (att in dataFile[COMPUTE_NODES[node]]){
 
         if (target_variables.includes(att)) {
 
-            vals = target_stat(dataFile[listOfNodes[node]][att]);
+            vals = target_stat(dataFile[COMPUTE_NODES[node]][att]);
 
-            if ((vals[0]>max && process.argv[3] != "min") || (process.argv[3] == "min" && vals[0] < min)) {
+            if ((vals[0] > max && process.argv[3] != "min") || 
+                (vals[0] < min && process.argv[3] == "min")) {
 
-                max = vals[0];
-                min = vals[0];
-                
-                if (process.argv[3] != "average" && process.argv[3] != "variance") {
-                    if (process.argv[3]=="sudden_change") reading = ", which is between reading " + vals[1] + " and reading" + (vals[1]+1) + " of ";
-                    else reading = ", which is on reading " + vals[1] + " of ";
-                }
-                else reading = ", which is in ";
+                    max = vals[0];
+                    min = vals[0];
+                    
+                    if (process.argv[3] != "average" && process.argv[3] != "variance") {
+                        if (process.argv[3]=="sudden_change") reading = ", which is between reading " + vals[1] + " and reading" + (vals[1]+1) + " of ";
+                        else reading = ", which is on reading " + vals[1] + " of ";
+                    }
 
-                if (target_variables.length == 1) equipment = listOfNodes[node] ;
-                else {
-                    if (target_variable == "temperature") equipment = att.substr(14, 4) + " of " + listOfNodes[node];
-                    if  (target_variable == "fan_speed") equipment = att.substr(3, 3) + att.substr(13, 1) + " of " + listOfNodes[node];
-                }   
+                    else reading = ", which is in ";
+
+                    if (target_variables.length == 1) equipment = COMPUTE_NODES[node] ;
+                    else {
+
+                        if (target_variable == "temperature") equipment = att.substr(14, 4) + " of " + COMPUTE_NODES[node];
+                        if  (target_variable == "fan_speed") equipment = att.substr(3, 3) + att.substr(13, 1) + " of " + COMPUTE_NODES[node];
+                    }   
             }
         }
     }
+
     if (process.argv[3] == "min") max = min;
 
 }
